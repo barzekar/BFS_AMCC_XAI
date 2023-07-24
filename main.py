5b4ad6566738b9d73cff09130601cba7f2c8bbe2
barzekar <barzekar@uwm.edu>
2023-07-23 23:06:51 -0500

    main file for non-Flask-based model running.

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+from bfs_amcc_runner import run_bfs_amcc
+
+
+def load_config():
+    """
+    Load the configuration file.
+    """
+    with open("config/bfs_amcc_config.json", "r") as file:
+        return json.load(file)
+
+
+def main():
+    config = load_config()
+    config["data_path"] = "data/" + config["data_path"]
+    metrics = run_bfs_amcc(config)
+    print(metrics)
+
+
+if __name__ == "__main__":
+    main()
